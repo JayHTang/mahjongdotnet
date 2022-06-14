fc2af4a0528faed94b8112509ae0a4dc2b4c78b0
jayhtang <jaytang8864@gmail.com>
2022-06-13 22:21:00 -0500

    Added reflection utility

---
+++ Mahjong/Utility/Reflection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace Mahjong.Utility
+{
+    class ReflectionUtility
+    {
+        public static object GetValue(object instance, string property, bool ignoreCase = false)
+        {
+            BindingFlags flags = ignoreCase ? defaultFlags | BindingFlags.IgnoreCase : defaultFlags;
+            return instance.GetType().GetProperty(property, flags).GetValue(instance);
+        }
+
+        public static void SetValue(object instance, string property, object value, bool ignoreCase = false)
+        {
+            BindingFlags flags = ignoreCase ? defaultFlags | BindingFlags.IgnoreCase : defaultFlags;
+            instance.GetType().GetProperty(property, flags).SetValue(instance, value);
+        }
+
+        public static void Increment(object instance, string property, int number = 1, bool ignoreCase = false)
+        {
+            int value = Convert.ToInt32(GetValue(instance, property, ignoreCase)) + number;
+            SetValue(instance, property, value, ignoreCase);
+        }
+
+        public static void Increment(object instance, string property, decimal number, bool ignoreCase = false)
+        {
+            decimal value = Convert.ToInt32(GetValue(instance, property, ignoreCase)) + number;
+            SetValue(instance, property, value, ignoreCase);
+        }
+
+        private static readonly BindingFlags defaultFlags = BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance;
+    }
+}
+
+
+
