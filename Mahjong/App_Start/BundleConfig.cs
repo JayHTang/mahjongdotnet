1883a156849e18b9e4bea4ada4f62c8fb489eb08
jayhtang <jaytang8864@gmail.com>
2022-06-14 23:27:25 -0500

    upgrade to bootstrap 5

--- Mahjong/App_Start/BundleConfig.cs
+++ Mahjong/App_Start/BundleConfig.cs
@@ -11,9 +11,6 @@ namespace Mahjong
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -22,7 +19,7 @@ namespace Mahjong
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
@@ -32,27 +29,12 @@ namespace Mahjong
                       "~/Content/css/bootstrap-datepicker3.standalone.css",
                       "~/Content/PagedList.css"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/jquery-ui").Include(
-                      "~/Content/themes/base/jquery-ui.min.css"));
-
             bundles.Add(new ScriptBundle("~/bundles/highcharts").Include(
                       "~/Scripts/highcharts/7.1.2/highcharts.js",
                       "~/Scripts/highcharts/7.1.2/highcharts-more.js",
                       "~/Scripts/highcharts/7.1.2/modules/data.js",
                       "~/Scripts/highcharts/7.1.2/modules/exporting.js",
                       "~/Scripts/highcharts/7.1.2/modules/drilldown.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/datepicker").Include(
-                      "~/Scripts/bootstrap-datepicker.js"));
-
-            bundles.Add(new StyleBundle("~/Content/gijgo/combined/gijgo").Include(
-                      "~/Content/gijgo/combined/gijgo.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/timepicker").Include(
-                      "~/Scripts/gijgo/combined/gijgo.js"/*,
-                      "~/Scripts/gijgo/modular/core.js",
-                      "~/Scripts/gijgo/modular/datepicker.js",
-                      "~/Scripts/gijgo/modular/timepicker.js"*/));
 
             BundleTable.EnableOptimizations = true;
 
